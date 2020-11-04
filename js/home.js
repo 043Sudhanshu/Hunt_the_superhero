c8d5060cd238864565a53d4e02dc660bdcb53658
sudhanshu chauhan <043sudhanshu@gmail.com>
2020-11-04 21:18:19 +0530

    sent http request on key press event and after parsing the response to object,i have appended the element to the div having cards id

---
+++ js/home.js
@@ -0,0 +1,47 @@
+(function(){
+     let search=document.getElementById('name');
+     let searchResults=document.getElementById('cards');
+     
+ search.onkeypress=function(){
+   var name=search.value;
+   $('#cards').text("");
+    var xhttp = new XMLHttpRequest(); 
+    xhttp.open("get",`https://www.superheroapi.com/api.php/3509711479121402/search/${name}`, true);
+    xhttp.send();
+    xhttp.onload = function() {   
+     
+        let result=JSON.parse(xhttp.response);
+       
+       if(result.response==='success'){
+                let array=result.results;
+                let i=1;
+                for(obj of array){
+                    if(i===10){
+                        break;
+                    }
+                    $('#cards').append(`
+                    
+                    <div class='card'>
+                    
+                            <div id='hero' style="display:none">
+                            ${JSON.stringify(obj)}
+                            </div>
+
+                            <img src='${obj.image.url}'>
+                                    <div class='cardName'>
+                                        ${obj.name}
+                                    </div>
+                   </div> 
+                   
+                    `);
+                i=i+1;
+                }
+       }
+    
+};
+    
+ }
+
+ 
+     
+})();
