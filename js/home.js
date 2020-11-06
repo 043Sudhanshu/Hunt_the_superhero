359b2bd91e55aa491b40fc76459bf03f568bef08
sudhanshu chauhan <043sudhanshu@gmail.com>
2020-11-07 03:51:43 +0530

    created a cube having the images of superheroes and used animation property to rotate it

--- js/home.js
+++ js/home.js
@@ -20,17 +20,20 @@
                         break;
                     }
                     $('#cards').append(`
-                    
+                  
                     <div class='card' id='id${obj.id}' onclick='setprofile(${obj.id})'>
                     
                             <div class='hero' style="display:none">${JSON.stringify(obj)}</div>
 
-                            <img src='${obj.image.url}'>
-                                    <div class='cardName'>
+                            <a href='./profile.html'>  
+                                <img src='${obj.image.url}'>
+                             
+                                <div class='cardName'>
                                         ${obj.name}
                                     </div>
+                                </a>
                    </div> 
-                   
+                  
                     `);
                 i=i+1;
                 }
@@ -49,6 +52,17 @@ var temp;
 function setprofile(cardId){
     var string="id"+cardId;
     var obj=$("#"+string+" "+".hero").text();
-    temp=JSON.parse(obj);
-    console.log(temp);
+    
+    localStorage.setItem("temp",obj);
   }
+
+
+  var str='Hi i am sudhanshu chauhan .i have used an superhero api by which i am fetching all the details of selected super hero jus typ the name of superhero above and enjoy :)';
+  var l=0;
+ var interval= setInterval(function(){
+        if(l==str.length){
+            clearInterval(interval);
+        }
+        $('#details>h4').text(str.substring(0,l)+"|");
+        l++;
+  },70);
--- js/profile.js
+++ js/profile.js
@@ -1,1 +1,11 @@
 
+var obj=localStorage.getItem("temp");
+
+var object=JSON.parse(obj);
+console.log(object);
+
+var p=0;
+setInterval(function(){
+$('#pic').css('transform',`rotate(${p}deg)`);
+p++;
+},50);
