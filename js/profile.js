b9465cfd741123769bfaeec205c055115be0d784
sudhanshu chauhan <043sudhanshu@gmail.com>
2020-11-13 01:50:55 +0530

    created profile page of the selected superhero

--- js/profile.js
+++ js/profile.js
@@ -4,8 +4,13 @@ var obj=localStorage.getItem("temp");
 var object=JSON.parse(obj);
 console.log(object);
 
-var p=0;
-setInterval(function(){
-$('#pic').css('transform',`rotate(${p}deg)`);
-p++;
-},50);
+$('#name').text(object.name);
+$('#pic>img').attr('src',object.image.url);
+setTimeout(function(){
+$('.power').width(object.powerstats.power+"%");
+$('.combat').width(object.powerstats.combat+"%");
+$('.intelligence').width(object.powerstats.intelligence+"%");
+$('.speed').width(object.powerstats.speed+"%");
+$('.strength').width(object.powerstats.strength+"%");
+$('.durability').width(object.powerstats.durability+"%");
+},500);
